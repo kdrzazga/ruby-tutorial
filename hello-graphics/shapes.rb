539a0c40ee449c9f4df6311ccb25ed57ad1b2322
kdrzazga <krzysztof.drzazga@tutanota.com>
2024-08-04 20:33:15 +0200

    hello-ruby (#1)
    * hello-ruby
    
    * hello-graphics
    
    * animations
    
    * added-sound
    
    ---------
    
    Co-authored-by: kdrzazga <kdrzazga@luxoft.com>

---
+++ hello-graphics/shapes.rb
@@ -0,0 +1,48 @@
+#gem install gosu  (this takes a few minutes)
+#gem install ruby2d
+
+require 'ruby2d'
+require 'gosu'
+
+class TriangleWindow < Gosu::Window
+  def initialize
+    super 640, 480
+    self.caption = "Gosu Triangle Example"
+	@x = 0
+	
+	@font = Gosu::Font.new(30, name: 'res/Bionic City.otf')
+	@image = Gosu::Image.new("res/plane.png")
+	@sound = Sound.new('res/plane.mp3')
+	@sound.play
+  end
+
+  def draw # this method is continuously called by default
+	Gosu.draw_rect(0, 0, self.width, self.height, Gosu::Color.argb(255, 0x35, 0x28, 0x79))
+
+    draw_triangle(100, 50,
+                  150, 150,
+                  50, 150)
+
+	@font.draw_text("Hello Gosu Graphics", 200, 200, 0, 1.0, 1.0, Gosu::Color::CYAN)
+	@image.draw(@x, 300, 0)
+
+	move_plane()
+  end
+  
+  def move_plane()
+	@x = @x + 1
+	
+	if @x > 630
+		@x = 0
+		@sound.stop
+	end
+  end
+
+  def draw_triangle(x1, y1, x2, y2, x3, y3)
+    Gosu.draw_line(x1, y1, Gosu::Color::WHITE, x2, y2, Gosu::Color::WHITE, 0)
+    Gosu.draw_line(x2, y2, Gosu::Color::WHITE, x3, y3, Gosu::Color::WHITE, 0)
+    Gosu.draw_line(x3, y3, Gosu::Color::WHITE, x1, y1, Gosu::Color::WHITE, 0)
+  end
+end
+
+TriangleWindow.new.show
---
+++ hello-ruby/hello.rb
@@ -0,0 +1,43 @@
+def distance(point1, point2)
+  raise ArgumentError, "Both points must be arrays of length 2." unless point1.length == 2 && point2.length == 2
+
+  x1, y1 = point1
+  x2, y2 = point2
+
+  Math.sqrt((x2 - x1)**2 + (y2 - y1)**2)
+end
+
+class Person
+  attr_accessor :name, :age
+
+  def initialize(name, age)  # constructor
+    @name = name
+    @age = age
+  end
+
+  def greet
+    "Hello, my name is #{@name} and I am #{@age} years old."
+  end
+
+  def birthday
+    @age += 1
+    "Happy Birthday! You are now #{@age} years old."
+  end
+end
+
+# ----------------------------------
+
+puts "Hello Ruby"
+puts ""
+puts "Some simple calculation:\n"
+
+point1 = [3, 4]
+point2 = [7, 1]
+
+dist = distance(point1, point2)
+puts "The distance between points #{point1} and #{point2} is #{dist}"
+
+person = Person.new("Alice", 30)
+puts person.greet
+puts person.birthday
+puts person.greet
