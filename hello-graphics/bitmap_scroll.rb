146b126f2817e7118314dae476c2dacab256984d
jerzy <jerzy.z.bialowiezy@proton.me>
2024-08-21 07:06:29 +0200

    introducing-Bar-class

--- hello-graphics/bitmap_scroll.rb
+++ hello-graphics/bitmap_scroll.rb
@@ -1,33 +1,47 @@
 require 'gosu'
 
+class Bar
+	attr_reader :bitmap, :y_position, :bitmap_height
+
+	def initialize
+		@bitmap = Gosu::Image.new("res/bitmap.png")
+		@y_position = 0
+		@bitmap_height = 15
+		@scroll_speed = 0.5
+	end
+	
+	def move
+		@y_position += @scroll_speed
+		if @y_position >= @bitmap_height
+			@y_position = 0
+		end
+	end
+end
+
+
 class ScrollingBitmapWindow < Gosu::Window
   def initialize
     super(800, 600, false)
     self.caption = "Scrolling Bitmap Animation"
+	
+	@bar = Bar.new()
 
-    @bitmap = Gosu::Image.new("res/bitmap.png")
-    @bitmap_height = 15
     @viewport_height = 15
-    @scroll_speed = 0.5
-    @y_position = 0
   end
 
   def update
-    @y_position += @scroll_speed
-    if @y_position >= @bitmap_height
-      @y_position = 0
-    end
+	@bar.move()
   end
 
   def draw
     draw_bitmap_section(0)
-    draw_bitmap_section(-@bitmap_height + @y_position)
+    draw_bitmap_section(-@bar.bitmap_height + @bar.y_position)
   end
 
   private
 
   def draw_bitmap_section(offset)
-    @bitmap.draw(0, offset, 0)
+    @bar.bitmap.draw(0, offset, 0)
   end
 end
 
